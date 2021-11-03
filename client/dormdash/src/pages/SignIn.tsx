42e1824391c928e2b530a9961b7e8a710139778e
Jamie-Lee Hart <71268169+pgm-jamihart@users.noreply.github.com>
2021-11-03 23:36:30 +0100

    added some styling to signin page

--- client/dormdash/src/pages/SignIn.tsx
+++ client/dormdash/src/pages/SignIn.tsx
@@ -1,13 +1,44 @@
 import React from "react";
+import styled from "styled-components";
 import { BaseLayout } from "../layouts";
+import backgroundImage from "../assets/SignInBg.png";
 
-interface Props {}
+const Container = styled.div`
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+`;
 
-const SignIn = (props: Props) => {
+const FormWrapper = styled.div`
+  z-index: 1;
+  position: relative;
+  margin-top: 5rem;
+`;
+
+const Image = styled.img`
+  display: none;
+
+  @media (min-width: ${(props) => props.theme.width.medium}) {
+    display: block;
+    width: 100%;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-55%);
+    left: 20%;
+  }
+`;
+
+const SignIn = () => {
   return (
     <BaseLayout>
-      <h1>Sign In</h1>
-      <p>Coming soon</p>
+      <Container>
+        <FormWrapper>
+          <h1>Welcome back</h1>
+          <p>Coming soon</p>
+        </FormWrapper>
+
+        <Image src={backgroundImage} />
+      </Container>
     </BaseLayout>
   );
 };
