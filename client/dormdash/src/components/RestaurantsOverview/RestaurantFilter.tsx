2e4dc00f15e9dfc19fab3a86177acd3d95ba7fde
Jamie-Lee Hart <71268169+pgm-jamihart@users.noreply.github.com>
2021-11-05 15:14:57 +0100

    finished styling category filter component

--- client/dormdash/src/components/RestaurantsOverview/RestaurantFilter.tsx
+++ client/dormdash/src/components/RestaurantsOverview/RestaurantFilter.tsx
@@ -1,20 +1,38 @@
 import React from "react";
 import Devider from "./Devider";
 import RestaurantFilterCategories from "./RestaurantFilterCategories";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const TitleContainer = styled.div`
+  color: ${(props) => props.theme.colors.primaryAccentColor};
+
+  span {
+    font-size: ${(props) => props.theme.fontSizes.emedium};
+    line-height: 1.2;
+
+    @media (min-width: ${(props) => props.theme.width.medium}) {
+      font-size: ${(props) => props.theme.fontSizes.large};
+    }
+  }
+`;
 
 interface Props {}
 
 const RestaurantFilter = (props: Props) => {
   return (
-    <div>
-      <h1>
-        <span>100</span>
-        Restaurants
-      </h1>
+    <Container>
+      <TitleContainer>
+        <h1>
+          <span>100 </span>
+          Restaurants
+        </h1>
+      </TitleContainer>
       <Devider />
       <RestaurantFilterCategories />
       <Devider />
-    </div>
+    </Container>
   );
 };
 
--- client/dormdash/src/components/RestaurantsOverview/RestaurantFilterCategories.tsx
+++ client/dormdash/src/components/RestaurantsOverview/RestaurantFilterCategories.tsx
@@ -8,13 +8,15 @@ interface Props {
 
 const Container = styled.div``;
 
-const TitleContainer = styled.div`
+const TitleContainer = styled.div<Props>`
   display: flex;
   align-items: center;
   justify-content: space-between;
   cursor: pointer;
 
   span {
+    transform: ${({ open }) => (open ? "rotate(180deg)" : "0")};
+    transition: all 0.2s ease;
     font-size: ${(props) => props.theme.fontSizes.medium};
     display: flex;
     align-items: center;
@@ -26,13 +28,13 @@ const ContainerChips = styled.div<Props>`
   display: flex;
   align-items: center;
   flex-wrap: wrap;
-  height: ${({ open }) => (open ? "0" : "auto")};
-  margin-top: ${({ open }) => (open ? "0" : "1rem")};
+  height: ${({ open }) => (open ? "auto" : "0")};
+  margin-top: ${({ open }) => (open ? "1rem" : "0")};
   transition: all 0.4s ease;
 `;
 
 const Chip = styled.button<Props>`
-  opacity: ${({ open }) => (open ? "0" : "1")};
+  opacity: ${({ open }) => (open ? "1" : "0")};
   transition: all 0.2s ease;
   background-color: ${(props) => props.theme.colors.secondaryAccentColor};
   display: flex;
@@ -44,7 +46,7 @@ const Chip = styled.button<Props>`
   border: none;
   padding: 0.25rem 1rem;
   border-radius: ${(props) => props.theme.borderRadius.large};
-  margin-bottom: ${({ open }) => (open ? "0" : "0.5rem")};
+  margin-bottom: ${({ open }) => (open ? "0.5rem" : "0")};
   margin-right: 0.5rem;
   cursor: pointer;
 `;
@@ -58,7 +60,7 @@ const RestaurantFilterCategories = () => {
 
   return (
     <Container>
-      <TitleContainer onClick={handleToggle}>
+      <TitleContainer onClick={handleToggle} open={open}>
         <h3>Categories</h3>
         <span>
           <MdExpandMore />
