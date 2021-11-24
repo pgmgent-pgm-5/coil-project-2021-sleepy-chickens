8f7d3e87cef74ab7e31a68d1a08c5f9f9d28f1e3
Jamie-Lee Hart <71268169+pgm-jamihart@users.noreply.github.com>
2021-11-24 19:11:34 +0100

    update

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,9 +5,22 @@ import Process from "../components/home/Process";
 import CallToActionDeliverer from "../components/home/CallToActionDeliverer";
 import CallToActionPartner from "../components/home/CallToActionPartner";
 import { Helmet } from "react-helmet";
+import { RestaurantSummaries } from "../interfaces/interfaces";
+import { useQuery } from "@apollo/client";
+import { RESTAURANTS_SUMMARY } from "../graphql/restaurants";
 interface Props {}
 
 const HomePage = (props: Props) => {
+  const { error, loading, data, refetch } = useQuery<RestaurantSummaries>(
+    RESTAURANTS_SUMMARY,
+    {
+      fetchPolicy: "cache-first",
+      variables: { province: String("Ohio") },
+    }
+  );
+
+  console.log(data);
+
   return (
     <BaseLayout>
       <Helmet>
