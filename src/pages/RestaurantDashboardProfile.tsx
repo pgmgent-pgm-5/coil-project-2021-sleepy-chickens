dd937d927e20b4a12d4190a43cee1e37fb82a2b8
Jamie-Lee Hart <71268169+pgm-jamihart@users.noreply.github.com>
2021-11-30 15:17:32 +0100

    fixing dashboard

--- src/pages/RestaurantDashboardProfile.tsx
+++ src/pages/RestaurantDashboardProfile.tsx
@@ -9,7 +9,11 @@ import PrimaryLink from "../components/Admin/Restaurant/PrimaryLink";
 import * as Routes from "../routes";
 import { Helmet } from "react-helmet";
 import { useUser } from "../context/AuthenticationContext";
-import { GET_RESTAURANTDETAIL_BY_USERID, RESTAURANTS_DETAIL, UPDATE_RESTAURANT } from "../graphql/restaurants";
+import {
+  GET_RESTAURANTDETAIL_BY_USERID,
+  RESTAURANTS_DETAIL,
+  UPDATE_RESTAURANT,
+} from "../graphql/restaurants";
 import { useLazyQuery, useMutation } from "@apollo/client";
 import { useEffect } from "react";
 import { Redirect } from "react-router-dom";
@@ -70,116 +74,126 @@ interface Props {}
 
 const RestaurantDashboardProfile = (props: Props) => {
   const userContext = useUser();
-  console.log('userId', userContext?.state.id);
+  console.log("userId", userContext?.state.id);
   const userId: number | undefined = userContext?.state.id;
 
-  const [restaurantDetailByUserId, { error, loading, data}] = useLazyQuery(GET_RESTAURANTDETAIL_BY_USERID);
+  const [restaurantDetailByUserId, { error, loading, data }] = useLazyQuery(
+    GET_RESTAURANTDETAIL_BY_USERID
+  );
 
-  const [updateRestaurant, {data:updateData, loading: updateLoading, error:updateError}] = useMutation(UPDATE_RESTAURANT);
+  const [
+    updateRestaurant,
+    { data: updateData, loading: updateLoading, error: updateError },
+  ] = useMutation(UPDATE_RESTAURANT);
 
   useEffect(() => {
     if (userId !== undefined) {
-      console.log('useeffectuserid', userId);
-      restaurantDetailByUserId({variables: {
-        userId: userId
-      }})
-
-
+      console.log("useeffectuserid", userId);
+      restaurantDetailByUserId({
+        variables: {
+          userId: userId,
+        },
+      });
     }
-  }, [userId, updateData])
+  }, [userId, updateData]);
 
   if (data) {
-    console.log('data data',data);
+    console.log("data data", data);
     return (
       <AdminLayout>
         <Helmet>
           <title>Dormdashboard | Profile</title>
           <meta name="description" content="edit profile" />
         </Helmet>
-  
-        <Container>
-          <Image>
-            <h1>Profile</h1>
-            <img src={`./assets/${data.getRestaurantByUserId.picture}`} alt="" />
-          </Image>
-          <FormikWrapper>
-            <Formik
-              // Veranderen naar values van current user
-              initialValues={{
-                restaurantName: data.getRestaurantByUserId.name,
-                description: data.getRestaurantByUserId.description,
-                image: "",
-              }}
-              onSubmit={(formData, { setSubmitting }) => {
-                setSubmitting(true);
-                console.log("restaurantIDDD", data.getRestaurantByUserId.id);
-  
-                updateRestaurant({
-                  variables: {
-                    id: data.getRestaurantByUserId.id,
-                    name: formData.restaurantName,
-                    description: formData.description,
-                    picture: 'default_menu.jpeg',
-                  },
-                  refetchQueries: [
-                    {
-                      query: RESTAURANTS_DETAIL,
-                      variables: { id: Number(data.getRestaurantByUserId.id)}
-                    }
-                  ]
-                })
 
-  
-                setSubmitting(false);
-              }}
-              validationSchema={validationSchema}
-            >
-              {({
-                values,
-                handleSubmit,
-                isSubmitting,
-                handleChange,
-                handleBlur,
-              }) => (
-                <form onSubmit={handleSubmit}>
-                  <Field
-                    type="text"
-                    as={InputField}
-                    name="restaurantName"
-                    placeholder={"Restaurant name"}
-                  />
-                  <Field
-                    as={Textarea}
-                    name="description"
-                    placeholder="Description"
-                  />
-                  <Field
-                    type="file"
-                    as={InputField}
-                    name="image"
-                    placeholder="Restaurant banner image"
-                  />
-                  <PrimaryButton disabled={isSubmitting} type="submit">
-                    Update
-                  </PrimaryButton>
-                  {/* <pre>{JSON.stringify(values, null, 2)}</pre> */}
-                </form>
-              )}
-            </Formik>
-          </FormikWrapper>
-          <LinkContainer>
-            <PrimaryLink link={Routes.DISHES}>Go back</PrimaryLink>
-          </LinkContainer>
-        </Container>
+        {data && (
+          <Container>
+            <Image>
+              <h1>Profile</h1>
+              <img
+                src={`./assets/${data.getRestaurantByUserId.picture}`}
+                alt=""
+              />
+            </Image>
+            <FormikWrapper>
+              <Formik
+                // Veranderen naar values van current user
+                initialValues={{
+                  restaurantName: data.getRestaurantByUserId.name,
+                  description: data.getRestaurantByUserId.description,
+                  image: "",
+                }}
+                onSubmit={(formData, { setSubmitting }) => {
+                  setSubmitting(true);
+                  console.log("restaurantIDDD", data.getRestaurantByUserId.id);
+
+                  updateRestaurant({
+                    variables: {
+                      id: data.getRestaurantByUserId.id,
+                      name: formData.restaurantName,
+                      description: formData.description,
+                      picture: "default_menu.jpeg",
+                    },
+                    refetchQueries: [
+                      {
+                        query: RESTAURANTS_DETAIL,
+                        variables: {
+                          id: Number(data.getRestaurantByUserId.id),
+                        },
+                      },
+                    ],
+                  });
+
+                  setSubmitting(false);
+                }}
+                validationSchema={validationSchema}
+              >
+                {({
+                  values,
+                  handleSubmit,
+                  isSubmitting,
+                  handleChange,
+                  handleBlur,
+                }) => (
+                  <form onSubmit={handleSubmit}>
+                    <Field
+                      type="text"
+                      as={InputField}
+                      name="restaurantName"
+                      placeholder={"Restaurant name"}
+                    />
+                    <Field
+                      as={Textarea}
+                      name="description"
+                      placeholder="Description"
+                    />
+                    <Field
+                      type="file"
+                      as={InputField}
+                      name="image"
+                      placeholder="Restaurant banner image"
+                    />
+                    <PrimaryButton disabled={isSubmitting} type="submit">
+                      Update
+                    </PrimaryButton>
+                    {/* <pre>{JSON.stringify(values, null, 2)}</pre> */}
+                  </form>
+                )}
+              </Formik>
+            </FormikWrapper>
+            <LinkContainer>
+              <PrimaryLink link={Routes.DISHES}>Go back</PrimaryLink>
+            </LinkContainer>
+          </Container>
+        )}
       </AdminLayout>
     );
-
-  } else {
-    return (
-      <Redirect to={Routes.ERROR.replace(':errorMessage', 'You are not authenticated!')} /> 
-    )
   }
-
+  //   else {
+  //     return (
+  //       <Redirect to={Routes.ERROR.replace(':errorMessage', 'You are not authenticated!')} />
+  //     )
+  //   }
 };
 
 export default RestaurantDashboardProfile;
